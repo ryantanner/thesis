21b97adf02f26ba775e79cab6284916c48e494f0
Ryan Tanner <Ryan@Hawkeye.local>
2011-09-04 16:51:00 -0500

    Cleaned up PTN.scala

--- stanford-corenlp-2011-06-19/ParseTreeNode.scala
+++ stanford-corenlp-2011-06-19/ParseTreeNode.scala
@@ -4,181 +4,164 @@ abstract class Sentence {
 	val dependencies: List[Dependency]
 	val parseTree: ParseTreeNode // holds parent
 	
-abstract class Token	{
-
-	val id: String
-	val word: String
-	val lemma: String
-	val begin: Int
-	val end: Int
-	val pos: String
-	val ner: String
+	abstract class Token	{
 	
-	override def toString = word
-	
-	def toXML =
-		<token id={id}>
-			<word>{word}</word>
-			<lemma>{lemma}</lemma>
-			<CharacterOffsetBegin>{begin}</CharacterOffsetBegin>
-			<CharacterOffsetEnd>{end}</CharacterOffsetEnd>
-			<POS>{pos}</POS>
-			<NER>{ner}</NER>
-		</token>
+		val id: String
+		val word: String
+		val lemma: String
+		val begin: Int
+		val end: Int
+		val pos: String
+		val ner: String
 		
-/*
-	def fromXML(node: scala.xml.Node): Token =
-		new Token {
-			val id = (node \ "@id").text
-			val word = (node \ "word").text
-			val lemma = (node \ "lemma").text
-			val begin = (node \ "CharacterOffsetBegin").text.toInt
-			val end = (node \ "CharacterOffsetEnd").text.toInt
-			val pos = (node \ "pos").text
-			val ner = (node \ "ner").text
-		}
-*/
-
+		override def toString = word
+		
+		def toXML =
+			<token id={id}>
+				<word>{word}</word>
+				<lemma>{lemma}</lemma>
+				<CharacterOffsetBegin>{begin}</CharacterOffsetBegin>
+				<CharacterOffsetEnd>{end}</CharacterOffsetEnd>
+				<POS>{pos}</POS>
+				<NER>{ner}</NER>
+			</token>
 			
-}
+	}
 
-object Token	{
+	object Token	{
+	
+		def fromXML(node: scala.xml.Node): Token =
+			new Token {
+				val id = (node \ "@id").text
+				val word = (node \ "word").text
+				val lemma = (node \ "lemma").text
+				val begin = (node \ "CharacterOffsetBegin").text.toInt
+				val end = (node \ "CharacterOffsetEnd").text.toInt
+				val pos = (node \ "pos").text
+				val ner = (node \ "ner").text
+			}
+		
+	}
 
-	def fromXML(node: scala.xml.Node): Token =
-		new Token {
-			val id = (node \ "@id").text
-			val word = (node \ "word").text
-			val lemma = (node \ "lemma").text
-			val begin = (node \ "CharacterOffsetBegin").text.toInt
-			val end = (node \ "CharacterOffsetEnd").text.toInt
-			val pos = (node \ "pos").text
-			val ner = (node \ "ner").text
+	abstract class Dependency	{
+	
+		val gov: Token 
+		val dep: Token 
+		val rel: String // dependency relation/type
+		
+		override def toString = "(" + gov + " > " + dep + " / " + rel + ")"
+	
+	}
+
+	object Dependency	{
+		
+		def fromXML(node: scala.xml.Node): Dependency =
+			new Dependency	{
+				val gov = tokens((node \ "governor" \ "@idx").text.toInt - 1)
+				val dep = tokens((node \ "dependent" \ "@idx").text.toInt - 1)
+				val rel = (node \ "@type") text
+			}
+	
+	}
+
+	class ParseTreeNode(par: ParseTreeNode, w: Token, p: String, i: Int)   {
+	
+	  val parent: ParseTreeNode = par
+	  //var word:String = w
+	  var pos:String = p
+	  var word:Token = w
+	  var childs:List[ParseTreeNode] = scala.collection.immutable.List()
+	  var index:Int = i
+	  var relations: List[Dependency] = List[Dependency]()
+	
+	  override def toString = word + "(" + index + ")" + "/" + pos + " " + childs.length + " children"
+	
+	  def this(p: String) = {
+		this(null,null,p,-1)
+	  }
+	
+	  def this() = {
+		this(null,null,"",-1)
+	  }
+	
+	  def this(par: ParseTreeNode) = {
+		this(par,null,"",-1)
+	  }
+	
+	  def :+(ptn: ParseTreeNode): ParseTreeNode = {
+		// add child right
+		childs = childs :+ ptn
+		return this
+	  }
+	
+	  def :+(par:ParseTreeNode, nw: Token, np: String): ParseTreeNode = {
+		childs = childs :+ new ParseTreeNode(par,nw,np,-1)
+		return this
+	  }
+	  
+	  def :+(par:ParseTreeNode, nw: Token, np: String, ni: Int): ParseTreeNode = {
+		childs = childs :+ new ParseTreeNode(par,nw,np,ni)
+		return this
+	  }
+	
+	  def +:(ptn: ParseTreeNode) {
+		childs = ptn +: childs
+	  }
+	
+	  def +:(par:ParseTreeNode, nw: Token, np: String) {
+		childs = (new ParseTreeNode(par,nw,np,-1)) +: childs
+	  }
+	
+	  def +:(par:ParseTreeNode, nw: Token, np: String, ni: Int) {
+		childs = (new ParseTreeNode(par,nw,np,ni)) +: childs
+	  }
+	
+	
+	}
+	
+	object ParseTreeNode	{
+		
+		def parse(parseString: String): ParseTreeNode = {
+			return pt_i(new ParseTreeNode, parseString.split(" ").toList,0)	
+		}
+		
+		// with index
+		private def pt_i(node: ParseTreeNode, elems:List[String], i:Int): ParseTreeNode = elems match {
+			case List() => node
+			case w :: rest => if ('(' == w.head) {
+					val child = new ParseTreeNode(node)
+					node :+ child
+					child.pos = w slice (1,w.length)
+					return pt_i(child,rest,i)
+				}
+				else {
+					node.word = tokens(i)
+					node.index = i
+					var n = node
+					for (i <- 1 to (w count { c => if (c == ')') true else false }))
+						n = n.parent
+					return pt_i(n,rest,i+1)
+				}
+		}
+		
+	
+		private def print_i(ptn:ParseTreeNode, i:Int): Int = {
+			 var s = new StringBuilder
+			 for (t <- 1 to i)
+				s append "\t"
+			 s append ptn.toString
+			 println(s)
+			 ptn.childs map { print_i(_,i+1) }
+			 return i
+		}
+		
+		def print(ptn: ParseTreeNode) = {
+			print_i(ptn,1)
 		}
 	
-}
-
-abstract class Dependency	{
-
-	val gov: Token 
-	val dep: Token 
-	val rel: String // dependency relation/type
 	
-	override def toString = "(" + gov + " > " + dep + " / " + rel + ")"
-
-		// only accept a stanford depenency node	
-
-	//def toXML
-}
-
-object Dependency	{
-	
-	def fromXML(node: scala.xml.Node): Dependency =
-		new Dependency	{
-			val gov = tokens((node \ "governor" \ "@idx").text.toInt - 1)
-			val dep = tokens((node \ "dependent" \ "@idx").text.toInt - 1)
-			val rel = (node \ "@type") text
-		}
-
-}
-
-class ParseTreeNode(par: ParseTreeNode, w: Token, p: String, i: Int)   {
-
-  val parent: ParseTreeNode = par
-  //var word:String = w
-  var pos:String = p
-  var word:Token = w
-  var childs:List[ParseTreeNode] = scala.collection.immutable.List()
-  var index:Int = i
-  var relations: List[Dependency] = List[Dependency]()
-
-  override def toString = word + "(" + index + ")" + "/" + pos + " " + childs.length + " children"
-
-  def this(p: String) = {
-    this(null,null,p,-1)
-  }
-
-  def this() = {
-    this(null,null,"",-1)
-  }
-
-  def this(par: ParseTreeNode) = {
-    this(par,null,"",-1)
-  }
-
-  def :+(ptn: ParseTreeNode): ParseTreeNode = {
-    // add child right
-    childs = childs :+ ptn
-    return this
-  }
-
-  def :+(par:ParseTreeNode, nw: Token, np: String): ParseTreeNode = {
-    childs = childs :+ new ParseTreeNode(par,nw,np,-1)
-    return this
-  }
-  
-  def :+(par:ParseTreeNode, nw: Token, np: String, ni: Int): ParseTreeNode = {
-    childs = childs :+ new ParseTreeNode(par,nw,np,ni)
-    return this
-  }
-
-  def +:(ptn: ParseTreeNode) {
-    childs = ptn +: childs
-  }
-
-  def +:(par:ParseTreeNode, nw: Token, np: String) {
-    childs = (new ParseTreeNode(par,nw,np,-1)) +: childs
-  }
-
-  def +:(par:ParseTreeNode, nw: Token, np: String, ni: Int) {
-    childs = (new ParseTreeNode(par,nw,np,ni)) +: childs
-  }
-
-
-}
-
-object ParseTreeNode	{
-	
-	def parse(parseString: String): ParseTreeNode = {
-		return pt_i(new ParseTreeNode, parseString.split(" ").toList,0)	
 	}
 	
-	// with index
-	private def pt_i(node: ParseTreeNode, elems:List[String], i:Int): ParseTreeNode = elems match {
-		case List() => node
-		case w :: rest => if ('(' == w.head) {
-				val child = new ParseTreeNode(node)
-				node :+ child
-				child.pos = w slice (1,w.length)
-				return pt_i(child,rest,i)
-			}
-			else {
-				node.word = tokens(i)
-				node.index = i
-				var n = node
-				for (i <- 1 to (w count { c => if (c == ')') true else false }))
-					n = n.parent
-				return pt_i(n,rest,i+1)
-			}
-	}
-	
-	
-	// perhaps split on ( as well, use pattern matching
-	// parent not being used correctly?
-	// write tab-delemited tree visualization for debugging!
-	
-	def print(ptn:ParseTreeNode, i:Int): Int = {
-		 var s = new StringBuilder
-		 for (t <- 1 to i)
-			s append "\t"
-		 s append ptn.toString
-		 println(s)
-		 ptn.childs map { printptn(_,i+1) }
-		 return i
-	}
-
-
-}
-
 
 
 }
@@ -200,66 +183,4 @@ object Sentence	{
 }
 
 
-
-
-//val parse = loadNode \\ "parse" text
-
-//var parseList = parse.split(" ").toList
-
-
-/*
-def pt(node: ParseTreeNode, elems:List[String]): ParseTreeNode = elems match {
-	case List() => node
-	case w :: rest => if ('(' == w.head) {
-			val child = new ParseTreeNode(node)
-			node :+ child
-			child.pos = w slice (1,w.length)
-			return pt(child,rest)
-		}
-		else {
-			node.word = Sentence.tokens(i)
-			var n = node
-			for (i <- 1 to (w count { c => if (c == ')') true else false }))
-				n = n.parent
-			return pt(n,rest)
-		}
-}
-*/
-
-
-// with index
-/*
-def pt_i(node: ParseTreeNode, elems:List[String], i:Int): ParseTreeNode = elems match {
-	case List() => node
-	case w :: rest => if ('(' == w.head) {
-			val child = new ParseTreeNode(node)
-			node :+ child
-			child.pos = w slice (1,w.length)
-			return pt_i(child,rest,i)
-		}
-		else {
-			node.word = Sentence.tokens(i)
-			node.index = i
-			var n = node
-			for (i <- 1 to (w count { c => if (c == ')') true else false }))
-				n = n.parent
-			return pt_i(n,rest,i+1)
-		}
-}
-
-
-// perhaps split on ( as well, use pattern matching
-// parent not being used correctly?
-// write tab-delemited tree visualization for debugging!
-
-def printptn(ptn:ParseTreeNode, i:Int): Int = {
-     var s = new StringBuilder
-     for (t <- 1 to i)
-     	s append "\t"
-     s append ptn.toString
-     println(s)
-     ptn.childs map { printptn(_,i+1) }
-     return i
-}
-*/
 
